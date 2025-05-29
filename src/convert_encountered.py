6a84d06f5ce3725b354f219583c224f394f69d7c
walther_knight <knightofargh@gmail.com>
2025-05-29 08:08:37 -0400

    Added conversion function for transitioning encountered to sourceable data format

--- src/convert_encountered.py
+++ src/convert_encountered.py
@@ -1,26 +1,12 @@
-from charmanager import *
-import convert_encountered
 
-def main():
-    #default character human with 12's
-    #char = CharacterNode()
-
-    #sample from Brett's character
-    '''char = CharacterNode("Marissa Lawson", 11, 12, 12, 13, 14, 15, 16)
-
-    print(char.name)
-    char.print_attributes()
-    char.print_move_rate()'''
-    
-    #quick convert for source file
-    input = "Unarmed,M,1/4/4,5d4,2,1d4MK,0|Bite,M,1/2/6,5d4,2,1d4+1MK,0"
-    convert_encountered.convert_encountered(input)
-    '''array = input.split("|")
+def convert_encountered(input):
+#quick convert for source file
+    #input = "Unarmed,M,1/4/4,5d4,2,1d4MK,0|Bite,M,1/2/6,5d4,2,1d4+1MK,0"
+    array = input.split("|")
     output = []
     workingFile = []
     for each in array:
         workingFile = each.split(",")
-        print(workingFile)
         del workingFile[3]
         workingFile[0] = "Name: " + workingFile[0]
         workingFile[1] = "Type: " + workingFile[1]
@@ -33,9 +19,4 @@ def main():
         else: 
             workingFile.append("Notes: ")
         output.append(",".join(workingFile))
-    print(output)'''
-    
-
-
-if __name__ == "__main__":
-        main()
+    print(output)
